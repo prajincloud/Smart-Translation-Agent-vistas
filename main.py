d648161bba66d37299a3daca23731f510f004349
Prajin Developer <prajin302007@gmail.com>
2026-01-18 06:10:23 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from deep_translator import GoogleTranslator
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
+)    
 
 class TranslationRequest(BaseModel):
     text: str
